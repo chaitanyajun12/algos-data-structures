3de5e606e64b234703f8d7ede49194ab5a690361
Krishna Chaitanya M <chaitanya.jun12@gmail.com>
2020-03-14 16:11:57 +0530

    Refactored the code the longest palindromic substring.

--- src/ds/strings/LongestPalindromicSubstring.java
+++ src/ds/strings/LongestPalindromicSubstring.java
@@ -2,54 +2,73 @@ package ds.strings;
 
 public class LongestPalindromicSubstring {
 
+    static class PalindromeData {
+        int palindromeStartIndex;
+        int palindromeLength;
+    }
+
+    public static void main(String[] args) {
+        
+        boolean result = getLongestPalindromeSubstring("") == "" &&
+                         getLongestPalindromeSubstring(null) == null &&
+                         getLongestPalindromeSubstring("a") == "a" && 
+                         getLongestPalindromeSubstring("abcd").equals("a") &&
+                         getLongestPalindromeSubstring("aabbcc").equals("aa");
+        System.out.println(result);
+    }
+
     /**
+     * Gets the longest palindromic substring by the logic of traversing each 
+     * character in the string assuming that it is the mid-point of the palindrome.
      * Time complexity: O(n^2)
      * Space complexity: O(n)
      */
-    public static void main(String[] args) {
-        String str = "abba";
+    static String getLongestPalindromeSubstring(String str) {
+
+        if (str == null || str.length() < 2)
+            return str;
+
         int N = str.length();
         int maxPalindromeLength = 0;
+        int startIndex = 0;
           
         for (int i = 0; i < N; i++) {
-            maxPalindromeLength = Math.max(maxPalindromeLength, 
-                                           expandRangeToFindPalindrome(str, i, i));
-            maxPalindromeLength = Math.max(maxPalindromeLength, 
-                                           expandRangeToFindPalindrome(str, i, i + 1));
+
+            PalindromeData pData = expandRangeToFindPalindrome(str, i, i);
+            if (pData.palindromeLength > maxPalindromeLength) {
+                startIndex = pData.palindromeStartIndex;
+                maxPalindromeLength = pData.palindromeLength;
+            }
+
+            pData = expandRangeToFindPalindrome(str, i, i + 1);
+            if (pData.palindromeLength > maxPalindromeLength) {
+                startIndex = pData.palindromeStartIndex;
+                maxPalindromeLength = pData.palindromeLength;
+            }
         }
-          
-        System.out.println("Max palindrome length: " + maxPalindromeLength);
-      }
         
-      static int expandRangeToFindPalindrome(String str, int start, int end) {
-          
-          int N = str.length();      
-          while (start >= 0 && end < N && str.charAt(start) == str.charAt(end)) {
-              start--;
-              end++;
-          }      
-          
-          return end - start - 1;
-      }
+        return str.substring(startIndex, maxPalindromeLength);
+    }
+
+    static PalindromeData expandRangeToFindPalindrome(String str, int start, int end) {
         
-      static boolean isPalindrome(String str) {
-    
-          if (str == null || str.length() < 2) 
-              return true;
-            
-          int start = 0;
-          int N = str.length();
-          int end = N - 1;
-                    
-          while(start <= end)
-          {
-            if (str.charAt(start) != str.charAt(end))
-                return false;
-    
-            start++;
-            end--;
-          }
-                    
-          return true;          
-      }    
+        int N = str.length();      
+        while (start >= 0 && end < N && str.charAt(start) == str.charAt(end)) {
+            start--;
+            end++;
+        }      
+        
+        PalindromeData pData = new PalindromeData();
+        pData.palindromeStartIndex = start + 1;
+        pData.palindromeLength = end - start - 1; // (end - 1) - (start + 1) + 1
+
+        return pData;
+    }
+        
+
+
+
+
+
+     
 }
